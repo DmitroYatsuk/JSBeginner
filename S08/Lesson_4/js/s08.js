2906d8002e20406462826ec02522f93c459afa90
DmitroYatsuk <dmytro.iatsuk@gmail.com>
2019-06-07 18:11:46 +0300

    White spaces have been removed

--- S08/Lesson_4/js/s08.js
+++ S08/Lesson_4/js/s08.js
@@ -3,18 +3,17 @@ for (var i = 0; i < 15; i++) {
     if (i == 8 || i == 13) {
         continue;
     }
-    var first = Math.floor((Math.random() * 6) + 1); 
-    var second = Math.floor((Math.random() * 6) + 1); 
+    var first = Math.floor((Math.random() * 6) + 1);
+    var second = Math.floor((Math.random() * 6) + 1);
     console.log("Первая кость: " + first + " Вторая кость: " + second);
     document.getElementById("result").innerHTML += "Первая кость: " + first + " Вторая кость: " + second + "<br>";
     if (first == second) {
         document.getElementById("result").innerHTML += "Выпал дубль. Число " + first + " <br>";
     }
-    else if (first < 3 && second > 4){
+    else if (first < 3 && second > 4) {
         document.getElementById("result").innerHTML += "Большой разброс между костями. Разница составляет: " + Math.abs(second - first) + "<br>";
     }
-
     total += first + second;
 }
-total > 100 ? document.getElementById("result").innerHTML += "Победа, вы набрали " + total + " очков" 
-            : document.getElementById("result").innerHTML += "Вы проиграли, у вас " + total + " очков";
+total > 100 ? document.getElementById("result").innerHTML += "Победа, вы набрали " + total + " очков"
+    : document.getElementById("result").innerHTML += "Вы проиграли, у вас " + total + " очков";
